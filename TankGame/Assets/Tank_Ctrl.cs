d1d4730c9dec3c0b6a1ee148ae2d5876e3c306b6
flzzyf <2981019122@qq.com>
2017-12-10 18:06:50 +0800

    换回老模型，开始加轮子，加移动函数

---
+++ TankGame/Assets/Tank_Ctrl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tank_Ctrl : MonoBehaviour {
+
+    public List<AxleInfo> axleInfos;
+
+    float motor = 0;
+    public float maxMotorTorque = 100;
+
+    float brakeTorque = 0;
+    public float maxBrakeTorque = 100;
+
+    float steer = 0;
+    public float maxSteerAngle = 40;
+
+    private void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        motor = v * maxMotorTorque;
+
+        steer = h * maxSteerAngle;
+
+        foreach (var item in axleInfos)
+        {
+            //转向轮
+            if(item.steer){
+                item.leftWheel.steerAngle = steer;
+                item.rightWheel.steerAngle = steer;
+            }
+            //动力轮
+            if(item.motor){
+                item.leftWheel.motorTorque = motor;
+                item.rightWheel.motorTorque = motor;
+            }
+        }
+
+    }
+
+
+}
