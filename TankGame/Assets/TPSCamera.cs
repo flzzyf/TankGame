1014694a74836b7730f2c7ae2810c35c1d17eb6f
flzzyf <2981019122@qq.com>
2017-12-13 15:26:16 +0800

    camera

---
+++ TankGame/Assets/TPSCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TPSCamera : MonoBehaviour {
+
+    public Transform target;
+
+    public Vector3 offset;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+
+        transform.position = target.position + offset;
+
+	}
+}
