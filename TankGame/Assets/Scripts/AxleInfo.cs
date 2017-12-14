58c9a3ac42056918583245619345e1906af1cc19
Flzzyf <flzzyf076@gmail.com>
2017-12-14 21:52:54 +0800

    换回高清坦克模型

---
+++ TankGame/Assets/Scripts/AxleInfo.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class AxleInfo{
+
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+
+    public bool motor;
+    public bool steer;
+
+}
